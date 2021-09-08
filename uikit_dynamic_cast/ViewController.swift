d7f120c4b7fef103d9811a65ebc47c575876ebb8
Ayxan Haqverdili <a.haqverdili@expressbank.az>
2021-09-08 15:23:57 +0400

    create xcode project

---
+++ uikit_dynamic_cast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit_dynamic_cast
+//
+//  Created by exb on 08.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
